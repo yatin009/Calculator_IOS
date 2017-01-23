21cdcd9f74758781dd3b8e5ef6a7a48feef2bf05
Yatin Sharma <yatin.sharma1991@gmail.com>
2017-01-23 14:48:59 -0500

    Calculation logic added

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var firstNumberText = "0.0"
+    var secondNumberText = "0.0"
+    var op = "+"
+    var isFirstNumber = true
+    var hasOp = false
+    var canClear = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +27,82 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func handleButtonPress(_ sender: UIButton) {
+        if canClear {
+            resultLabel.text = ""
+            canClear = false
+        }
+        let currentText = resultLabel.text!
+        let textLabel = sender.titleLabel?.text
+        if let text = textLabel {
+            switch text {
+            case "+", "X", "/", "-":
+                if hasOp {
+                    return
+                }
+                op = text
+                isFirstNumber = false
+                hasOp = true
+                resultLabel.text = "\(currentText) \(op) "
+                break
+            case "sqr":
+                isFirstNumber = true
+                hasOp = false
+                canClear = true
+                let result = calculateSquareRoot()
+                resultLabel.text = "\(result)"
+                break
+            case "=":
+                isFirstNumber = true
+                hasOp = false
+                canClear = true
+                let result = calculate()
+                resultLabel.text = "\(result)"
+                break
+            case "AC":
+                resultLabel.text = "0.0"
+                isFirstNumber = true
+                hasOp = false
+                canClear = true
+                break
+            default:
+                if isFirstNumber {
+                    firstNumberText = "\(firstNumberText)\(text)"
+                } else {
+                    secondNumberText = "\(secondNumberText)\(text)"
+                }
+                resultLabel.text = "\(currentText)\(text)"
+                break;
+            }
+        }
+    }
+    
+    func calculateSquareRoot() -> Double{
+        let firstNumber = Double(firstNumberText)!
+        firstNumberText = ""
+        return sqrt(firstNumber)
+    }
+    
+    func calculate() -> Double {
+        let firstNumber = Double(firstNumberText)!
+        let secondNumber = Double(secondNumberText)!
+        firstNumberText = ""
+        secondNumberText = ""
+        switch op {
+        case "+":
+            return firstNumber + secondNumber
+        case "-":
+            return firstNumber - secondNumber
+        case "X":
+            return firstNumber * secondNumber
+        case "/":
+            return firstNumber / secondNumber
+        default:
+            return 0
+        }
     }
 
 
